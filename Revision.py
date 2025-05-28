44003c2788c2086909c2e605baf6854700f63373
Danish Mir <76698292+mirdanish6594@users.noreply.github.com>
2025-05-28 10:31:29 +0530

    revision

--- Revision.py
+++ Revision.py
@@ -1,24 +1,26 @@
 from typing import List
 
 class Solution:
-    def reverseArray(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        k = k % n
+    def mergedInterval(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+        
+        intervals.sort(key=lambda x: x[0])
+        merged = [intervals[0]]
 
-        def reverse(start: int, end: int):
-            while start < end:
-                nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
+        for current in intervals[1: ]:
+            last = merged[-1]
+            if current[0] <= last[1]:
+                last[1] = max(last[1], current[1])
+            else:
+                merged.append(current)
 
-        reverse(0, n-1)
-        reverse(0, k-1)
-        reverse(k, n-1)
-
+        return merged
+    
 if __name__ == "__main__":
-    nums = list(map(int, input("Enter the array: ").split()))
-    k = int(input("Enter the value of k: "))
+    nums = list(map(int, input("Enter the intervals in spaced way: ").strip().split()))
+    intervals = [nums[i: i+2] for i in range(0, len(nums), 2)]
 
     sol = Solution()
-    sol.reverseArray(nums, k)
-    print("The reversed array is: ", nums)
+    result = sol.mergedInterval(intervals)
+    print("Merged Intervals: ", result)
